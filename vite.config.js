465c6cfceb1f8df3bf7cfc98fd399903204b1445
DonnyRZ <donny.landscape@gmail.com>
2025-04-02 06:57:35 +0700

    Configure base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-/**
- * @type {import('vite').UserConfig}
- */
-export default {
-    build: {
-      sourcemap: true,
-    },
-  };
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Add this line, replacing 'minecraft-demo' with YOUR repository name
+  base: '/MarsWarArena/',
+  
+  build: {
+    sourcemap: true,
+  },
+});
